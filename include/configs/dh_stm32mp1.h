e4cd3c1c45bce954f442aa7f71be6866046793ec
Martina Schmoeller <mschmoeller@dh-electronics.com>
2020-11-27 18:08:11 +0100

    dh_stm32mp1: Automatic check for bootenv file on USB-Flash

--- include/configs/dh_stm32mp1.h
+++ include/configs/dh_stm32mp1.h
@@ -11,5 +11,27 @@
 #include <configs/stm32mp1.h>
 
 #define CONFIG_SPL_TARGET		"u-boot.itb"
+#define STM32MP_BOOTCMD "bootcmd_stm32mp=" \
+	"if run load_bootenv; then run importbootenv;fi;" \
+	"echo \"Boot over ${boot_device}${boot_instance}!\";" \
+	"if test ${boot_device} = serial || test ${boot_device} = usb;" \
+	"then stm32prog ${boot_device} ${boot_instance}; " \
+	"else " \
+		"run env_check;" \
+		"if test ${boot_device} = mmc;" \
+		"then env set boot_targets \"mmc${boot_instance}\"; fi;" \
+		"if test ${boot_device} = nand ||" \
+		  " test ${boot_device} = spi-nand ;" \
+		"then env set boot_targets ubifs0; fi;" \
+		"run distro_bootcmd;" \
+	"fi;\0"
 
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	CONFIG_EXTRA_ENV_SETTINGS \
+	"load_bootenv="\
+	"load usb ${usbdev}:${usbpart} ${loadaddr} DHupdate.ini;" \
+	"echo \"--> Update: found DHupdate.ini (${filesize} bytes)\"; \0"\
+	"importbootenv=echo Importing environment from DHupdate.ini...; env import -t ${loadaddr} ${filesize}\0" \
 #endif
+
+
