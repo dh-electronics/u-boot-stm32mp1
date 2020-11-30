32acd3625f50f1164be94c0194bf8a691a92324c
Martina Schmoeller <mschmoeller@dh-electronics.com>
2020-11-30 10:59:32 +0100

    DH-STM32mp1: Add extra environment for DHCOM

--- include/configs/dh_stm32mp1.h
+++ include/configs/dh_stm32mp1.h
@@ -11,24 +11,8 @@
 #include <configs/stm32mp1.h>
 
 #define CONFIG_SPL_TARGET		"u-boot.itb"
-#undef STM32MP_BOOTCMD
-#define STM32MP_BOOTCMD "bootcmd_stm32mp=" \
-	"if run load_bootenv; then run importbootenv;fi;" \
-	"echo \"Boot over ${boot_device}${boot_instance}!\";" \
-	"if test ${boot_device} = serial || test ${boot_device} = usb;" \
-	"then stm32prog ${boot_device} ${boot_instance}; " \
-	"else " \
-		"run env_check;" \
-		"if test ${boot_device} = mmc;" \
-		"then env set boot_targets \"mmc${boot_instance}\"; fi;" \
-		"if test ${boot_device} = nand ||" \
-		  " test ${boot_device} = spi-nand ;" \
-		"then env set boot_targets ubifs0; fi;" \
-		"run distro_bootcmd;" \
-	"fi;\0" 
 
-#undef CONFIG_EXTRA_ENV_SETTINGS
-#define CONFIG_EXTRA_ENV_SETTINGS \
+#define CONFIG_DH_EXTRA_ENV_SETTINGS \
 	"bootdelay=1\0" \
 	"kernel_addr_r=0xc2000000\0" \
 	"fdt_addr_r=0xc4000000\0" \
@@ -58,5 +42,6 @@
 	"load usb ${usbdev}:${usbpart} ${loadaddr} DHupdate.ini;" \
 	"echo \"--> Update: found DHupdate.ini (${filesize} bytes)\"; \0"\
 	"importbootenv=echo Importing environment from DHupdate.ini...; env import -t ${loadaddr} ${filesize}\0"
+
 #endif
 
